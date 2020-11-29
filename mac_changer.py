650e203683dbe51535b32f85c77614afcc5bc22e
lulze <51793738+lulzeDD@users.noreply.github.com>
2020-11-29 07:30:45 +0100

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -2,11 +2,11 @@
 
 import subprocess
 
-interface = input("Enter interface : ") # change input to raw_input for python2
-new_mac = input("Enter new mac : ") # change input to raw_input for python2
+interface = raw_input("Enter interface : ")
+new_mac = raw_input("Enter new mac : ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
