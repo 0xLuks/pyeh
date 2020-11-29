9ab70a7d950f94ffe1462809ac666874d13b8141
lulze <51793738+lulzeDD@users.noreply.github.com>
2020-11-29 17:22:08 +0100

    Update network_scan.py

--- network_scan.py
+++ network_scan.py
@@ -1,15 +1,24 @@
 # Tested on Kali 2020.1
+# Example
+# python network_scan.py -r x.x.x.x/24
 
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import optparse
+
+
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-r", "--range", dest="range", help="IP range.")
+    options, arguments = parser.parse_args()
+    return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-
     clients_list = []
     for element in answered_list:
         client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
@@ -21,5 +30,8 @@ def print_result(results_list):
     for client in results_list:
         print(client["ip"] + "\t\t" + client["mac"])
 
-scan_result = scan("RANGE_IP")
+
+options = get_args()
+scan_result = scan(options.range)
 print_result(scan_result)
+
